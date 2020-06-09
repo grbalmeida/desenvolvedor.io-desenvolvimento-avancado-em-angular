4c168aa46916da6d9d14d426a1ab2a2f9862a257
gilvan <g.r.almeida@live.com>
2020-06-09 12:58:58 -0300

    Módulos de roteamento

--- Arquitetura de Componentes/src/app/app.routes.ts
+++ Arquitetura de Componentes/src/app/app.routes.ts
@@ -1,11 +1,22 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './navegacao/home/home.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.component';
 
-export const rootRouterConfig: Routes = [
+const rootRouterConfig: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full'},
     { path: 'home', component: HomeComponent},
     { path: 'sobre', component: SobreComponent },
     { path: 'cadastro', component: CadastroComponent }
 ];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(rootRouterConfig)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule { }
