745b7ce6ed07999d0aec51f4c9fbf97330750f06
gilvan <g.r.almeida@live.com>
2020-06-11 11:11:30 -0300

    Aplicando Pipes no Angular

---
+++ Pipes/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
+
+import { NgBrazil, TextMask } from 'ng-brazil';
+import { CustomFormsModule } from 'ng2-validation';
+
+import { AppComponent } from './app.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.component';
+import { NavegacaoModule } from './navegacao/navegacao.module';
+import { AppRoutingModule } from './app.routes';
+import { AuthGuard } from './services/app.guard';
+import { CadastroGuard } from './services/cadastro.guard';
+import { FilmesComponent } from './demos/pipes/filmes/filmes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SobreComponent,
+    CadastroComponent,
+    FilmesComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NavegacaoModule,
+    TextMask.TextMaskModule,
+    NgBrazil,
+    CustomFormsModule,
+    AppRoutingModule
+  ],
+  providers: [
+    AuthGuard,
+    CadastroGuard
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
