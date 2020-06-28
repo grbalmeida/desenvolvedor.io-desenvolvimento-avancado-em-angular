dc0752273b46d356039a94af9cffc87b9aa79589
gilvan <g.r.almeida@live.com>
2020-06-28 20:51:15 -0300

    Exibição de mapas

--- ProjetoFinal/src/app/fornecedor/detalhes/detalhes.component.ts
+++ ProjetoFinal/src/app/fornecedor/detalhes/detalhes.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
+
 import { Fornecedor } from '../models/fornecedor';
 
-import { ActivatedRoute } from '@angular/router';
-import { FornecedorService } from '../services/fornecedor.service';
 
 @Component({
   selector: 'app-detalhes',
@@ -11,10 +12,21 @@ import { FornecedorService } from '../services/fornecedor.service';
 export class DetalhesComponent {
 
   fornecedor: Fornecedor = new Fornecedor();
+  enderecoMap: any;
+  chave = 'AIzaSyAP0WKpL7uTRHGKWyakgQXbW6FUhrrA5pE';
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private sanitizer: DomSanitizer
   ) {
     this.fornecedor = this.route.snapshot.data.fornecedor;
+    this.enderecoMap = this.sanitizer.bypassSecurityTrustResourceUrl(
+      `https://www.google.com/maps/embed/v1/place?q=${this.EnderecoCompleto}&key=${this.chave}`
+    );
+  }
+
+  get EnderecoCompleto(): string {
+    const { street, number: addressNumber, district, city, state } = this.fornecedor.address;
+    return `${street}, ${addressNumber} - ${district}, ${city} - ${state}`;
   }
 }
